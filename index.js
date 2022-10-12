38c3a2d1d739f9daa291b218e992c8a956c04e24
Ryan Ridwan <ryanridnn@gmail.com>
2022-10-12 15:47:37 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+app.get("/", async (req, res) => {
+  const resp = await axios({
+    url: "https://api.webflow.com/collections/6331444324771a7fc18695e9/items",
+    method: "GET",
+    headers: {
+      Authorization:
+        "Bearer 8da4c236c0f49a9ad5e1e47a39edcf45db3e338c13cbf14d67c3abd078675fad",
+    },
+  });
+
+  res.status(200).json(resp.data);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server has started on PORT ${process.env.PORT || 3000}`);
+});
